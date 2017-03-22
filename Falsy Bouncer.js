cbc94b03e53aed0162d963b05910a1b5b9a28a26
NicholasBraga <nicholaslbsebastiao@gmail.com>
2017-03-22 14:00:12 -0300

    Update Falsy Bouncer.js

--- Falsy Bouncer.js
+++ Falsy Bouncer.js
@@ -1,3 +1,7 @@
+/*Remove all falsy values from an array.
+
+Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.*/
+
 function bouncer(arr) {
   var newarr = arr.filter(function(val){	//Retornará tudo o que for diferente de...
   	return val !== "" &&
