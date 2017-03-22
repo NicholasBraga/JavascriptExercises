d459d47f1fba3766b58ac092795529501774a86d
NicholasBraga <nicholaslbsebastiao@gmail.com>
2017-03-22 14:06:25 -0300

    Update Return Largest Numbers in Arrays.js

--- Return Largest Numbers in Arrays.js
+++ Return Largest Numbers in Arrays.js
@@ -1,29 +1,5 @@
-
-/*function largestOfFour(arr) {
-var i = 0;
-var j = 0;
-var aux = 0;
-var maiorvalor = 0;
-var testearray = [];
-
-
-
-for(i in arr){
-	console.log("Nova Iteração de I:");
-	for(j in arr[i]){
-		if(arr[i][j] >= maiorvalor){
-			maiorvalor = arr[i][j];
-			//console.log(testearray);
-		}
-	}
-	testearray[i] = maiorvalor;
-	console.log("Impressão do 'testearray': " + testearray);
-}
-
-  //return aux;
-}
-
-largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);*/
+/*Return an array consisting of the largest number from each provided sub-array.
+For simplicity, the provided array will contain exactly 4 sub-arrays.*/
 
 function largestOfFour(arr) {
 var i = 0;
@@ -34,20 +10,19 @@ var testearray = [];
 
 
 
-for(i in arr){
-	console.log("Nova Iteração de I:");
-	for(j in arr[i]){
-		if(arr[i][j] >= maiorvalor){
-			maiorvalor = arr[i][j];
+for(i in arr){  //Laço mais lento - vai iterar o "i" só quando todo o "j" tiver sido percorrido
+	//console.log("Nova Iteração de I:");
+	for(j in arr[i]){  //Laço mais rápido - vai percorrer todos os itens da dimensão atual do array
+		if(arr[i][j] >= maiorvalor){  //Se o item da posição atual do array for maior ou igual que "maiorvalor"...
+			maiorvalor = arr[i][j];  //"maiorvalor" recebe o item
 			//console.log(testearray);
 		}
 	}
-	testearray[i] = maiorvalor;
-	console.log("Impressão do 'testearray': " + testearray);
-	maiorvalor = 0;
+	testearray[i] = maiorvalor;  //O array só receberá o maior valor de cada dimensão, pois ele está localizado fora do laço "j" que é o mais rápido.
+	maiorvalor = 0;  //"maiorvalor" é zerado toda vez que o laço "j" acabar e outro laço "i" for começar, para não carregar o valor prévio para a próxima iteração.
 }
 
-  //return aux;
+  return testearray;
 }
 
 largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
