cfc2bda8cd4891be85ca1cc6a5a5bec71392649e
NicholasBraga <nicholaslbsebastiao@gmail.com>
2017-03-22 14:16:24 -0300

    Update Where do I Belong.js

--- Where do I Belong.js
+++ Where do I Belong.js
@@ -1,5 +1,8 @@
+/*Return the lowest index at which a value (second argument)should be inserted into an array 
+(first argument) once it has been sorted. The returned value should be a number.*/
+
 //Função serve para corrigir falha do método sort() que converte os valores em strings e os organiza de acordo com a tabela Unicode, o que pode acarretar numa ordenação numérica errada
-/*function organize(a, b){
+function organize(a, b){
   return a-b;
 }
 
@@ -9,29 +12,14 @@ function getIndexToIns(arr, num) {
   for(var i = 0; i < newarr.length; i++){ //Percorre o array da esquerda para a direita
     if(num > newarr[newarr.length-1]){  //Checa se "num" é maior do que o último valor do array
       newarr[newarr.length] = num;
-      console.log(newarr.indexOf(num));
       return newarr.indexOf(num);
     }else{
       if(num >= newarr[i] && num < newarr[i+1]){  //Caso não seja maior que o último valor, procura onde ele se encaixa
         newarr[newarr.length] = num;
         newarr.sort(organize);
-        console.log(newarr.indexOf(num));
-        console.log(newarr);
         return newarr.indexOf(num);
       }
     }
   }
 }
-getIndexToIns([2, 5, 10], 6);*/
-
-function organize(a, b){
-  return a - b;
-}
-
-function getIndexToIns(arr, num) {
-  arr[arr.length] = num;  //Cria um novo slot no array para receber o valor da variável "num"
-  var newarr = arr.sort(organize);  //Array de apoio que recebe o array original organizado por sort() e com o método organize() para evitar problemas inerentes ao sort(), já que ele converte os valores em Strings e os organiza pelo código Unicode, o que pode causar erro na sequência numérica
-  return newarr.indexOf(num);
-}
-
-getIndexToIns([2, 5, 10], 15);
+getIndexToIns([2, 5, 10], 6);
